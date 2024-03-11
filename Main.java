06ff6d9956eabc81b282c54dab6be223411a541c
IlyaSevastyanov <145929492+IlyaSevastyanov@users.noreply.github.com>
2024-03-12 00:59:11 +0300

    Переделаны некоторые методы. Добавлены java.doc

--- Main.java
+++ Main.java
@@ -1,34 +1,64 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
 
+import java.util.Scanner;
+/**
+ * Основной класс программы.
+ */
 public class Main {
+
+    /**
+     * Точка входа в программу. Считывает строки с консоли, фильтрует их по длине и выводит отфильтрованный список строк.
+     * @param args аргументы командной строки (не используются)
+     */
     public static void main(String[] args) {
 
         String[] strings = readStringsFromConsole();
-        List<String> filteredStrings = filterStringsByLength(strings, 3);
-        System.out.println("Отфильтрованный список строк (длина <= 3): " + filteredStrings);
+        String[] filteredStrings = filterStringsByLength(strings, 3);
+        System.out.print("Отфильтрованный список строк (длина <= 3): ");
+        for (String str : filteredStrings) {
+            System.out.print(str + " ");
+        }
 
     }
-
-    public static List<String> filterStringsByLength(String[] strings, int maxLength) {
-        List<String> filteredStrings = new ArrayList<>();
-
+    /**
+     * Фильтрует массив строк по длине строки.
+     * @param strings массив строк для фильтрации
+     * @param maxLength максимальная длина строки для фильтрации
+     * @return массив отфильтрованных строк
+     */
+    public static String[] filterStringsByLength(String[] strings, int maxLength) {
+        int count = 0;
         for (String str : strings) {
             if (str.length() <= maxLength) {
-                filteredStrings.add(str);
+                count++;
+            }
+        }
+
+        String[] filteredStrings = new String[count];
+        int index = 0;
+        for (String str : strings) {
+            if (str.length() <= maxLength) {
+                filteredStrings[index] = str;
+                index++;
             }
         }
 
         return filteredStrings;
     }
 
+    /**
+     * Считывает строки с консоли.
+     * @return массив строк, введенных с консоли
+     * @throws IllegalArgumentException если количество строк меньше или равно 0
+     */
     public static String[] readStringsFromConsole() {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Введите количество строк: ");
         int count = scanner.nextInt();
-        scanner.nextLine(); // Считываем символ новой строки после ввода числа
+        scanner.nextLine();
 
+        if (count <= 0) {
+            throw new IllegalArgumentException("Количество строк должно быть больше нуля.");
+        }
         String[] strings = new String[count];
         for (int i = 0; i < count; i++) {
             System.out.print("Введите строку " + (i + 1) + ": ");
